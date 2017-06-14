9c40386c8a0cc0d11ec74b4104e6acd3bafb93bc
David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-06-13 21:23:50 -0600

    separate print tests from wrapper, add led_mat_library files

---
+++ library/led_mat_library.h
@@ -0,0 +1,17 @@
+#ifndef LED_MAT_LIB_H
+#define LED_MAT_LIB_H
+
+#ifndef STDIO
+#define STDIO
+#include <stdio.h>
+#endif /* STDIO */
+
+#ifndef TEXT_PRINT
+#define TEXT_PRINT
+#include "text_print.h"
+#endif /* TEXT_PRINT */
+
+void
+print ( int argc, char ** argv );
+
+#endif /* LED_MAT_LIB_H */
---
+++ library/text_print.h
@@ -0,0 +1,15 @@
+#ifndef TEXT_PRINT_H
+#define TEXT_PRINT_H
+
+#ifndef STDIO
+#define STDIO
+#include <stdio.h>
+#endif /* STDIO */
+
+void
+tabbed_print ( char * text, int tab_number );
+
+void
+print_newline ( void );
+
+#endif /* TEXT_PRINT_H */
