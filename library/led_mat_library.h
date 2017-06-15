d20822dbf5cd47a5080d6280807bd532ce4ad8da
David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-06-15 00:47:00 -0600

    add delay to the device setting

--- library/led_mat_library.h
+++ library/led_mat_library.h
@@ -31,11 +31,6 @@
 #include <unistd.h>
 #endif /* UNISTD */
 
-#ifndef TEXT_PRINT
-#define TEXT_PRINT
-#include "text_print.h"
-#endif /* TEXT_PRINT */
-
 #ifndef GLIB
 #define GLIB
 #include <glib.h>
@@ -76,6 +71,24 @@
 #include <sys/stat.h>
 #endif /* SYS_STAT */
 
+#ifndef TIME
+#define TIME
+#include <time.h>
+#endif /* TIME */
+
+#ifndef TEXT_PRINT
+#define TEXT_PRINT
+#include "text_print.h"
+#endif /* TEXT_PRINT */
+
+#ifndef DEV_FILENAME
+#define DEV_FILENAME "/sys/bus/i2c/drivers/ledMatc/1-0070/mat_led"
+#endif /* DEV_FILENAME */
+
+#ifndef FILENAME
+#define FILENAME "/dev/i2c-1"
+#endif /* FILENAME */
+
 #ifndef LED_MAT_ADDRESS
 #define LED_MAT_ADDRESS 0x70
 #endif /* LED_MAT_ADDRESS */
@@ -84,9 +97,17 @@
 #define LED_MAT_PRIME_REG 0x00
 #endif /* LED_MAT_PRIME_REG */
 
+#ifndef LED_BLINK_REG
+#define LED_BLINK_REG 0x81
+#endif /* LED_BLINK_REG */
+
 #ifndef SYMBOL_LENGTH
 #define SYMBOL_LENGTH 8
 #endif /* SYMBOL_LENGTH */
+
+#ifndef WRITE_BUFFER_SIZE
+#define WRITE_BUFFER_SIZE 2
+#endif /* BYTE_WRITE_BUFFER_SIZE */
 
 #ifndef A_CHAR
 #define A_CHAR
@@ -103,6 +124,25 @@ uint8_t A[8] =
   };
 #endif /* A_CHAR */
 
+/* Wait a specified amount of milliseconds. */
+void
+wait( unsigned long milliseconds );
+
+void
+write_byte ( int * dev_fd, uint8_t dev_register, uint8_t data );
+
+void
+set_blink_off ( void );
+
+void
+open_device ( int * dev_fd );
+
+void
+close_device ( int * dev_fd );
+
+void
+set_device_as_slave ( int * dev_fd );
+
 /* Display a symbol on the LED Matrix. */
 void
 display_symbol ( uint8_t * symbol_data );
@@ -116,8 +156,7 @@ void
 led_mat_led_array_on ( uint8_t * array_state, uint8_t length );
 
 /* High level abstract text print function, uses the previous functions
-   to "print" characters in the LED Matrix.
- */
+   to "print" characters in the LED Matrix. */
 void
 led_mat_print ( int argc, char ** argv );
 
